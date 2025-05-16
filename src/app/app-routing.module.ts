6e3f716b834f8fa6d7c07b8169942ebdd5beace4
n.torresg23 <n.torresg23@uniandes.edu.co>
2025-05-16 07:51:58 -0500

    Punto 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: RecipeListComponent},
+  {path: "/recipe", loadChildren: () => import ("./recipe/recipe.module").then(m => m.RecipeModule)},
+  {path: "/recipe/:id", loadChildren: () => import("./recipe/recipe.module").then(m => m.RecipeModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
